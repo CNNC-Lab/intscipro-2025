582365fbbe2d8dc369e81fac68a070f1879df6d8
Renato <rcfduarte@gmail.com>
2025-10-30 14:05:16 +0000

    feat: add age calculator scripts with input validation

---
+++ datasets/test.py
@@ -0,0 +1,8 @@
+my_age  = input('How old are you?  ')
+my_age_int = int(my_age)	 
+
+if my_age_int > 100:
+    print()
+else:
+    years_to_go  = 100 - my_age_int	
+    print(f'You will turn 100 in {years_to_go} years.')	
---
+++ day01-introduction/demo_scripts/test.py
@@ -0,0 +1,4 @@
+my_age  = input('How old are you?')
+my_age_int = int(my_age)	 
+years_to_go  = 100 - my_age_int	
+print(f'You will turn 100 in {years_to_go} years.')	 
